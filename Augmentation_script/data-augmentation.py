76bec16bf9afdb223e2e07b42d656b64dcbe9080
naveen kumar <chandra.naveen523@gmail.com>
2021-04-27 23:32:43 +0100

    Create data-augmentation.py

---
+++ Augmentation_script/data-augmentation.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+The script used for randomly generating augmented data for over sampling. 
+keras experimental preprocessing layers are utilized in the program for randomly augmenting the data and saving it.
+
+"""
+
+
+import random
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os
+import tensorflow as tf
+
+from tensorflow.keras.layers.experimental.preprocessing import RandomContrast,RandomRotation,RandomCrop,RandomFlip
+
+
+transform = tf.keras.Sequential([
+    RandomRotation(0.2, fill_mode='reflect', interpolation='bilinear'),
+    RandomFlip("horizontal_and_vertical"),
+    RandomContrast(0.2),
+    RandomCrop(220, 220)
+])
+
+
+
+folder_path = 'D:\MSC\Computer_Vision\Labs\Cropped_images'
+Desired_no_of_files = 500
+
+Save_folder = 'D:\MSC\Computer_Vision\Labs\Cropped_images'
+# find all files paths from the folder
+images = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+
+generated_files = 1
+while generated_files <= Desired_no_of_files:
+    # randomly selecting an image
+    image_path = random.choice(images)
+
+    # Read an image with OpenCV and convert it to the RGB colorspace
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    #Expanding the dimensions of the image for passing it on to keras preprocessing layers
+    image = tf.expand_dims(image, 0)
+
+    # Augmenting the image
+    augment = transform(image)
+    transformed_image = augment[0]
+
+    new_file_path = '%s/augmented_image_%s.jpg' % (Save_folder, generated_files)
+    cv2.imwrite(new_file_path, transformed_image)
+
+    generated_files += 1
+
+
+
+
+
